7e33a0f312d76678bb379cbffd651c78ce7f95f2
dmamills <d.mills@vehikl.com>
2025-04-01 16:36:11 -0400

    wip

--- publishers/slack-publisher.js
+++ publishers/slack-publisher.js
@@ -1,84 +1,77 @@
 
 const SLACK_TOKEN = process.env.SLACK_TOKEN;
 
+
+const currentListBlock = (currentList) => ({
+  "type": "section",
+  "text": {
+    "type": "mrkdwn",
+    "text": `*Current List:*\n ${currentList.map((n,idx) => `- ${n} ${idx == currentList.length-1 ? '(*Next Up*)':''}`).join('\n')}`
+  } 
+})
+
 class SlackPublisher {
   constructor(publisher) {
     publisher.sub('register', this.onAdded.bind(this))
     publisher.sub('removed', this.onRemoved.bind(this))
     publisher.sub('assignment', this.onAssign.bind(this))
-
   }
 
   onAdded({ addedName, currentList }) {
-    const blocks = {
-      blocks: [
+    const blocks =  [
         { 
           "type": "section",
           "text": {
             "type": "mrkdwn",
-            "text": `${addedName} has been added to the rotation!`
+            "text": `*${addedName}* has been added to the rotation!`
           }
         },
         {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": `Current List:\n ${currentList.map(n => `- ${n}`).join('\n')}`
-          }
-        }
+          "type": "divider"
+        },
+        currentListBlock(currentList)
       ]
-    }
     return this.dispatchPost(blocks);
   }
 
   onAssign({ assignedUser, assigned, currentList }) {
-    const blocks = {
-      blocks: [
+    const blocks = [
         { 
           "type": "section",
           "text": {
             "type": "mrkdwn",
-            "text": `${assignedUser} you're up!`
+            "text": `*${assignedUser}* you're up!`
           }
         },
         {
           "type": "section",
           "text": {
             "type": "mrkdwn",
-            "text": `Assigned MRs:\n ${assigned.map(n => `- ${n}`).join('\n')}`
+            "text": `*Assigned MRs:*\n ${assigned.map(n => `- <${n}|${n}>`).join('\n')}`
           }
         },
         {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": `Current List:\n ${currentList.map(n => `- ${n}`).join('\n')}`
-          }
-        }
+          "type": "divider"
+        },
+        currentListBlock(currentList)
       ]
-    }
     return this.dispatchPost(blocks);
   }
 
   async onRemoved({ removedName, currentList }) {
-    const blocks = {
-      blocks: [
+    const blocks = [
         { 
           "type": "section",
           "text": {
             "type": "mrkdwn",
-            "text": `${removedName} has been removed from the rotation!`
+            "text": `*${removedName}* has been removed from the rotation!`
           }
         },
         {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": `Current List:\n ${currentList.map(n => `- ${n}`).join('\n')}`
-          }
-        }
+          "type": "divider"
+        },
+        currentListBlock(currentList)
       ]
-    }
     return this.dispatchPost(blocks);
   }
 
@@ -87,7 +80,7 @@ class SlackPublisher {
     return fetch(SLACK_TOKEN, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(blocks),
+      body: JSON.stringify({ blocks }),
     }).then((response) => response.status);
   }
 }
